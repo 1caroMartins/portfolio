7feb66289ecb7192e803d3efe135567a9650381a
Icaro Martins <112215685+1caroMartins@users.noreply.github.com>
2023-02-25 11:20:00 -0300

    configurando formulário 2

--- assets/script.js
+++ assets/script.js
@@ -120,3 +120,31 @@ if(target.length) {
         animeScroll()
     },10))
 }
+
+//Mensagem formulário
+function message(){
+    var Name = document.getElementById('name');
+    var email = document.getElementById('email');
+    var subject = document.getElementById('subject');
+    var msg = document.getElementById('msg');
+    const success = document.getElementById('success');
+    const danger = document.getElementById('danger');
+
+    if(Name.value === '' || email.value === '' || subject.value === '' || msg.value === ''){
+        danger.style.display = 'block';
+    }else{
+        setTimeout(() => {
+            Name.value = '';
+            email.value = '';
+            subject.value = '';
+            msg.value = '';
+        }, 2000);
+
+        success.style.display = 'block';
+    }
+
+    setTimeout(() => {
+        danger.style.display = 'none';
+        success.style.display = 'none';
+    }, 4000);    
+}
